a8858280bad354d8de7df2f2e949cff68a76a2d7
aldairryan20@gmail.com <aldairryan20@gmail.com>
2024-03-15 16:51:34 -0300

    Stack class

--- func.py
+++ func.py
@@ -1,14 +1,11 @@
-def sumImpar(k):
-    if k<10:
-        if k%2 == 0:
-            return 0
-        else:
-            return k
-    else:
-        if k%2 == 0:
-            return sumImpar(k//10)
-        else:
-            return sumImpar(k//10) + k%10
-# print(sumImpar(123456789))
-# >>> 25
+import classes as c
+'''Implemente uma função que transfira os dados de uma pilha S
+para uma pilha T, de tal forma que o topo da pilha S seja o
+primeiro elemento da pilha T, e o último elemento de S seja
+o topo da pilha T.'''
 
+def trnsfstk(s, t):
+    r = c.Stack(s.Capacity)
+    r.push(s.pop())
+    print(r.peek(), s.peek())
+    return s, t
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import classes as c
-L = c.LinkedList()
-L.insertAtBeginning(25)
-L.insertAtBeginning(1)
-L.printList()
+import func as f
+s = c.Stack(3)
+t = c.Stack(3)
+
+s.push(7)
+t.push(5)
+s.peek()
+s.peek()
